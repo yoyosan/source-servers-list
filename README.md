1f5ca0c1f30a3f224c4097fecad310c42a26c3a7
Yoyosan <veselu@gmail.com>
2015-06-02 23:39:17 +0300

    .

---
+++ README.md
@@ -0,0 +1,19 @@
+Purpose
+=======
+
+Get a list of all the servers(and players connected).
+It will stop when the 0.0.0.0 server address is reached.
+
+Install
+=======
+
+```
+git submodule update --init --recursive
+```
+
+Run
+===
+
+```
+python2 list.py
+```
--- list.py
+++ list.py
@@ -1,1 +1,40 @@
-__author__ = 'yoyosan'
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libs/python_valve'))
+
+from valve.source import master_server
+from valve.source import a2s
+from valve.source import messages
+
+msq = master_server.MasterServerQuerier()
+stop_address = u"0.0.0.0"
+address = u"1.2.3.4"
+no_servers = 0
+
+while address != stop_address:
+    try:
+        for address in msq.find(region=[u"all"],
+                                gamedir=u"csgo",
+                                gametype=[u"idle", u"secure"]
+                                ):
+            try:
+                server = a2s.ServerQuerier(address)
+                info = server.get_info()
+                players = server.get_players()
+
+                print "Address: %s:%s" % (address[0], address[1]), " - {player_count}/{max_players} - {map}".format(
+                    **info), info["server_name"], "\n"
+                for player in sorted(players["players"],
+                                     key=lambda p: p["score"], reverse=True):
+                    print "\t", "{score}".format(**player), player["name"]
+
+                no_servers += 1
+            except messages.BrokenMessageError:
+                continue
+
+    except a2s.NoResponseError:
+        print "Master server request timed out!"
+        continue
+
+print 'Number of servers', no_servers
