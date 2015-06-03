e3b04f40cd7dda244950d67e775a7527ca0db426
Yoyosan <veselu@gmail.com>
2015-06-03 22:25:41 +0300

    Add bash run script.

--- list.sh
+++ list.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-workon
+export WORKON_HOME=~/Envs
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2
+source /usr/bin/virtualenvwrapper.sh
+
+workon source-servers-list
+`which python2` list.py
